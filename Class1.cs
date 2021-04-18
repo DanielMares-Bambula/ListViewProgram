41b53392fc2403b0021aa1e3e38d2ff3d312e36d
DanielMares-Bambula <79077162+DanielMares-Bambula@users.noreply.github.com>
2021-04-18 21:59:36 +0200

    Přidejte soubory projektu.

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ListViewProgram
+{
+    class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string dateOfBirth;
+        public string DateOfBirth
+        {
+            get => dateOfBirth;
+            set { dateOfBirth = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DateOfBirth")); }
+        }
+        private int birthNumber;
+        public int BirthNumber
+        {
+            get => birthNumber;
+            set { birthNumber = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BirthNumber")); }
+        }
+        public static Dictionary<string, Person> AllPeople = new Dictionary<string, Person>();
+        public static void InitPersons()
+        {
+            AllPeople["Tonda Houška"] = new Person
+            {
+                DateOfBirth = "13. June 2002",
+                BirthNumber = 25263732,
+
+            };
+            AllPeople["Lucie Houšková"] = new Person
+            {
+                DateOfBirth = "20. April 2005",
+                BirthNumber = 4631635,
+
+            };
+        }
+    }
+}
